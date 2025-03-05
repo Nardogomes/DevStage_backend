fb1e349201d86032d5f08adf417826a78970abcc
Ednardo Gomes Dos Santos <ednardo.santos@lanlink.com.br>
2025-03-05 17:49:58 -0300

    core: Initializing project, installing fastify, typescript and zod.

---
+++ src/server.ts
@@ -0,0 +1,34 @@
+import { fastify } from 'fastify'
+import { fastifyCors } from '@fastify/cors'
+import { validatorCompiler, serializerCompiler, ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
+
+app.register(fastifyCors, {
+  origin: true,
+})
+
+app.get('/hello', () => {
+  return { hello: 'world' }
+})
+
+app.post('/subscriptions', {
+  schema: {
+    body: z.object({ 
+      name: z.string().min(3).max(255),
+      email: z.string().email()
+    }),
+  },
+}, (request, reply) => {
+  const { name, email } = request.body
+
+  return reply.status(201).send({ name, email })
+})
+
+app.listen({ port: 3333 }, () => {
+  console.log('Server running at http://localhost:3333/')
+})
