c2f31195a1e08a71321896b1d467cdf5fb11232b
Ednardo Gomes Dos Santos <ednardo.santos@lanlink.com.br>
2025-03-11 21:03:33 -0300

    feat: swagger configuration and creation of the subscribe-to-evente.ts file

--- src/server.ts
+++ src/server.ts
@@ -4,10 +4,11 @@ import {
   validatorCompiler,
   serializerCompiler,
   ZodTypeProvider,
+  jsonSchemaTransform
 } from "fastify-type-provider-zod";
-import { z } from "zod";
 import { fastifySwagger } from "@fastify/swagger";
 import { fastifySwaggerUi } from "@fastify/swagger-ui";
+import { subscribeToEventRoute } from "./routes/subscribe-to-event-route";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
@@ -18,34 +19,25 @@ app.register(fastifyCors, {
   origin: true,
 });
 
-app.register(fastifySwagger);
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Dev Stage',
+      version: '0.0.1'
+    }
+  },
+  transform: jsonSchemaTransform
+});
+
+app.register(fastifySwaggerUi, {
+  routePrefix: '/docs'
+})
 
 app.get("/hello", () => {
   return { hello: "world" };
 });
 
-app.post(
-  "/subscriptions",
-  {
-    schema: {
-      body: z.object({
-        name: z.string().min(3).max(255),
-        email: z.string().email(),
-      }),
-      response: {
-        201: z.object({
-          name: z.string().min(3).max(255),
-          email: z.string().email(),
-        }),
-      },
-    },
-  },
-  async (request, reply) => {
-    const { name, email } = request.body;
-
-    return reply.status(201).send({ name, email });
-  }
-);
+app.register(subscribeToEventRoute);
 
 app.listen({ port: 3333 }, () => {
   console.log("Server running at http://localhost:3333/");
